e89e0d10980beb1dfab0c8acac88e7a06ac6b6e5
Brian Du <brian.bh.du@gmail.com>
2017-04-09 17:06:41 -0400

    CRM Web: Saving your Data  1.1
    - Closed database connection
    - git ignored committing the database

--- contact.rb
+++ contact.rb
@@ -12,10 +12,15 @@ require 'mini_record'
 
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: 'crm-web.sqlite3')
 
-class Contact
+class Contact <ActiveRecord::Base
 
-  attr_reader :id
-  attr_accessor :first_name, :last_name, :email, :note
+  field :first_name, as: :string
+  field :last_name,  as: :string
+  field :email,      as: :string
+  field :note,       as: :text
+
+  # attr_reader :id
+  # attr_accessor :first_name, :last_name, :email, :note
 
   @@contacts = []
   @@id = 1
@@ -110,3 +115,5 @@ class Contact
   end
 
 end
+
+Contact.auto_upgrade!
